a6f51fcac7b59e856761874ec39e6f0e5338a208
Anotnio <lateralprogramming@gmail.com>
2021-02-10 15:40:48 -0800

    initial commit

---
+++ new_stuff.py
@@ -0,0 +1,9 @@
+def house():
+    l = int(input("What's the length of your house? "))
+    w = int(input("Whats's the width of your house? "))
+    print(l*w)
+
+from math import pi
+def cir():
+    r = int(input("What's the radius of your circle? "))
+    print(f'The circumfrance is {2*pi*r}')
